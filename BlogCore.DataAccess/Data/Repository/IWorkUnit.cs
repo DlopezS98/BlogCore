78775373a50ab9168ac020c8c1f6acc0fc200c0a
DlopezS98 <01dlopezs98@gmail.com>
2020-09-20 20:54:56 -0600

    creating unit of work repository (IWorkUnit.cs, WorkUnit.cs)

---
+++ BlogCore.DataAccess/Data/Repository/IWorkUnit.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogCore.DataAccess.Data.Repository
+{
+    public interface IWorkUnit : IDisposable
+    {
+        ICategoryRepository Category { get; }
+
+        void Save();
+    }
+}
---
+++ BlogCore.DataAccess/Data/WorkUnit.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using BlogCore.DataAccess.Data.Repository;
+
+namespace BlogCore.DataAccess.Data
+{
+    public class WorkUnit : IWorkUnit
+    {
+        private readonly ApplicationDbContext db;
+        public WorkUnit(ApplicationDbContext _db)
+        {
+            this.db = _db;
+            Category = new CategoryRepository(_db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
